a158b9ac1c45d4cda631c636d03acddb864cef7e
Raj Tulluri <mail.rajtulluri@gmail.com>
2020-05-20 11:26:10 +0530

    Text on image added

--- app/src/main/java/com/example/memeapp/ImageAdapter.java
+++ app/src/main/java/com/example/memeapp/ImageAdapter.java
@@ -35,7 +35,7 @@ public class ImageAdapter extends BaseAdapter{
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(985, 1200));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(28, 15, 8, 15);
+            imageView.setPadding(28, 18, 8, 15);
         }
         else
         {
@@ -49,7 +49,20 @@ public class ImageAdapter extends BaseAdapter{
             R.drawable.img0, R.drawable.img1, R.drawable.img2,
             R.drawable.img3, R.drawable.img4, R.drawable.img5,
             R.drawable.img6, R.drawable.img7, R.drawable.img8,
-            R.drawable.img9
+            R.drawable.img9, R.drawable.img10, R.drawable.img11, R.drawable.img12,
+            R.drawable.img13, R.drawable.img14, R.drawable.img15,
+            R.drawable.img16, R.drawable.img17, R.drawable.img18,
+            R.drawable.img19, R.drawable.img20, R.drawable.img21, R.drawable.img22,
+            R.drawable.img23, R.drawable.img24, R.drawable.img25,
+            R.drawable.img26, R.drawable.img27, R.drawable.img28,
+            R.drawable.img29, R.drawable.img30, R.drawable.img31, R.drawable.img32,
+            R.drawable.img33, R.drawable.img34, R.drawable.img35,
+            R.drawable.img36, R.drawable.img37, R.drawable.img38,
+            R.drawable.img39, R.drawable.img40, R.drawable.img41, R.drawable.img42,
+            R.drawable.img43, R.drawable.img44, R.drawable.img45,
+            R.drawable.img46, R.drawable.img47, R.drawable.img48,
+            R.drawable.img49, R.drawable.img50, R.drawable.img51, R.drawable.img52,
+            R.drawable.img53
     };
 
 }
--- app/src/main/java/com/example/memeapp/Main2Activity.java
+++ app/src/main/java/com/example/memeapp/Main2Activity.java
@@ -10,9 +10,16 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,7 +30,11 @@ import static android.media.MediaScannerConnection.scanFile;
 
 public class Main2Activity extends AppCompatActivity {
 
-    public ImageView img;
+    private Uri passImageUri;
+    ImageButton processImage;
+    EditText addTextEditText;
+    private static int IMAGE_REQ = 44;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +50,26 @@ public class Main2Activity extends AppCompatActivity {
         int pos = i.getExtras().getInt("id");
         ImageAdapter ia = new ImageAdapter(this);
 
-        ImageView iv = findViewById(R.id.imageView3);
-        iv.setImageResource(ia.mThumbIds[pos]);
-        img= iv;
+        imageView = findViewById(R.id.imageView3);
+        imageView.setImageResource(ia.mThumbIds[pos]);
+
+        processImage =  findViewById(R.id.imageButton2);
+        addTextEditText = findViewById(R.id.editText);
+
+        processImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(addTextEditText.getText().toString()))
+                {
+                    addTextEditText.setError("Enter the text");
+                }else
+                {
+                    String text = addTextEditText.getText().toString();
+                    addTextEditText.setText("");
+                    addTextOnImage(text);
+                }
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
@@ -56,6 +84,54 @@ public class Main2Activity extends AppCompatActivity {
 
     public void download_image(View view){
 //        Download image code
+    }
+
+    private void addTextOnImage(String text) {
+
+        //pass the data to add it in image
+        Intent intent = new Intent(Main2Activity.this,TextOnImage.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(TextOnImage.IMAGE_IN_URI,passImageUri.toString()); //image uri
+        bundle.putString(TextOnImage.TEXT_COLOR,"#27ceb8");                 //initial color of the text
+        bundle.putFloat(TextOnImage.TEXT_FONT_SIZE,20.0f);                  //initial text size
+        bundle.putString(TextOnImage.TEXT_TO_WRITE,text);                   //text to be add in the image
+        intent.putExtras(bundle);
+        startActivityForResult(intent, TextOnImage.TEXT_ON_IMAGE_REQUEST_CODE); //start activity for the result
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == IMAGE_REQ)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                passImageUri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), passImageUri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(requestCode == TextOnImage.TEXT_ON_IMAGE_REQUEST_CODE)
+        {
+            if(resultCode == TextOnImage.TEXT_ON_IMAGE_RESULT_OK_CODE)
+            {
+                Uri resultImageUri = Uri.parse(data.getStringExtra(TextOnImage.IMAGE_OUT_URI));
+                try {
+                    Bitmap  bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultImageUri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else if(resultCode == TextOnImage.TEXT_ON_IMAGE_RESULT_FAILED_CODE)
+            {
+                String errorInfo = data.getStringExtra(TextOnImage.IMAGE_OUT_ERROR);
+                Log.d("MainActivity", "onActivityResult: "+errorInfo);
+            }
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 
 }
