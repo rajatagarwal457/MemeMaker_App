57c207f70d916e4c2f4768200794c1b67d090e47
Raj Tulluri <mail.rajtulluri@gmail.com>
2020-05-22 19:31:45 +0530

    added ranked retrieval

--- app/src/main/java/com/example/memeapp/Main3Activity.java
+++ app/src/main/java/com/example/memeapp/Main3Activity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 public class Main3Activity extends AppCompatActivity {
     int pos;
+    int[] arr;
     ImageView imageView;
     ImageAdapter ia;
 
@@ -23,9 +24,18 @@ public class Main3Activity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+//        Intent i = getIntent();
+//        pos = i.getExtras().getInt("id");
+//        ia = new ImageAdapter(this);
+
         Intent i = getIntent();
-        pos = i.getExtras().getInt("id");
         ia = new ImageAdapter(this);
+        pos = i.getExtras().getInt("id");
+        String str = i.getExtras().getString("pos_arr");
+        if(str != null) {
+            arr = parse_String(str);
+            sort_res(arr);
+        }
 
         imageView = findViewById(R.id.imageView4);
         imageView.setImageResource(ia.mExmaples[pos]);
@@ -39,5 +49,32 @@ public class Main3Activity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public void sort_res(int[] arr){
+        int size = arr.length;
+        for(int i=0; i<size; i++){
+            int tmp = ia.mThumbIds[i];
+            ia.mThumbIds[i] = ia.mThumbIds[arr[i]];
+            ia.mThumbIds[arr[i]] = tmp;
+
+            tmp = ia.mExmaples[i];
+            ia.mExmaples[i] = ia.mExmaples[arr[i]];
+            ia.mExmaples[arr[i]] = tmp;
+        }
+    }
+
+    public int[] parse_String(String input){
+        String output;
+        output = input.replace("[","");
+        output = output.replace("]","");
+        String[] str_array = output.split(",");
+        int size = str_array.length;
+        int[] arr = new int[size-1];
+        for(int i=0; i<size-1; i++){
+            str_array[i] = str_array[i].replaceAll("\\s","");
+            arr[i] = Integer.parseInt(str_array[i]);
+        }
+        return arr;
     }
 }
